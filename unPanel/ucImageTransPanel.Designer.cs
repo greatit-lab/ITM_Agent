029f765f9c75020310fdc2389510aa451141373f
greatit-lab <gil2zum@gmail.com>
2024-12-31 10:16:58 +0900

    Update ucImageTransPanel.Designer.cs

--- unPanel/ucImageTransPanel.Designer.cs
+++ unPanel/ucImageTransPanel.Designer.cs
@@ -1,6 +1,6 @@
 namespace ITM_Agent.ucPanel
 {
-    partial class ucScreen3
+    partial class ucImageTransPanel
     {
         private System.ComponentModel.IContainer components = null;
         
@@ -16,13 +16,13 @@ namespace ITM_Agent.ucPanel
         private System.Windows.Forms.SplitContainer splitContainer2;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cb_TargetImageFolder;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button btn_SelectOutputFolder;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox cb_WaitTime;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Label label2;
@@ -33,17 +33,17 @@ namespace ITM_Agent.ucPanel
         {
             this.splitContainer2 = new System.Windows.Forms.SplitContainer();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.button5 = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.button3 = new System.Windows.Forms.Button();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.button6 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.cb_WaitTime = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cb_TargetImageFolder = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.btn_SelectOutputFolder = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
             this.splitContainer2.Panel1.SuspendLayout();
             this.splitContainer2.Panel2.SuspendLayout();
@@ -65,7 +65,7 @@ namespace ITM_Agent.ucPanel
             // splitContainer2.Panel2
             //
             this.splitContainer2.Panel2.Controls.Add(this.groupBox2);
-            this.splitContainer2.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer2_Panel2_Paint);
+            //this.splitContainer2.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer2_Panel2_Paint);
             this.splitContainer2.Size = new System.Drawing.Size(632, 304);
             this.splitContainer2.SplitterDistance = 128;
             this.splitContainer2.TabIndex = 3;
@@ -87,51 +87,23 @@ namespace ITM_Agent.ucPanel
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "● Set a Condition";
             //
-            // button1
+            // button6
             //
-            this.button1.Location = new System.Drawing.Point(506, 23);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(112, 20);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Clear";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button6.Location = new System.Drawing.Point(373, 65);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(127, 20);
+            this.button6.TabIndex = 12;
+            this.button6.Text = "Set Time";
+            this.button6.UseVisualStyleBackColor = true;
             //
-            // comboBox1
+            // button4
             //
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(170, 23);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(184, 20);
-            this.comboBox1.TabIndex = 0;
-            //
-            // groupBox2
-            //
-            this.groupBox2.Controls.Add(this.label2);
-            this.groupBox2.Controls.Add(this.button5);
-            this.groupBox2.Location = new System.Drawing.Point(3, 6);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(624, 156);
-            this.groupBox2.TabIndex = 0;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "● Image Save Folder";
-            //
-            // button5
-            //
-            this.button5.Location = new System.Drawing.Point(373, 65);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(245, 28);
-            this.button5.TabIndex = 5;
-            this.button5.Text = "Select Folder";
-            this.button5.UseVisualStyleBackColor = true;
-            //
-            // label3
-            //
-            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.label3.Location = new System.Drawing.Point(18, 27);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(146, 22);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "Target Image Folder";
+            this.button4.Location = new System.Drawing.Point(373, 22);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(127, 20);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Set Folder";
+            this.button4.UseVisualStyleBackColor = true;
             //
             // label1
             //
@@ -151,31 +123,50 @@ namespace ITM_Agent.ucPanel
             this.button3.Text = "Clear";
             this.button3.UseVisualStyleBackColor = true;
             //
-            // comboBox2
+            // cb_WaitTime
             //
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(170, 65);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(184, 20);
-            this.comboBox2.TabIndex = 8;
+            this.cb_WaitTime.FormattingEnabled = true;
+            this.cb_WaitTime.Location = new System.Drawing.Point(170, 65);
+            this.cb_WaitTime.Name = "cb_WaitTime";
+            this.cb_WaitTime.Size = new System.Drawing.Size(184, 20);
+            this.cb_WaitTime.TabIndex = 8;
             //
-            // button4
+            // label3
             //
-            this.button4.Location = new System.Drawing.Point(373, 22);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(127, 20);
-            this.button4.TabIndex = 11;
-            this.button4.Text = "Set Folder";
-            this.button4.UseVisualStyleBackColor = true;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3.Location = new System.Drawing.Point(18, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(146, 22);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Target Image Folder";
             //
-            // button6
+            // button1
             //
-            this.button6.Location = new System.Drawing.Point(373, 65);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(127, 20);
-            this.button6.TabIndex = 12;
-            this.button6.Text = "Set Time";
-            this.button6.UseVisualStyleBackColor = true;
+            this.button1.Location = new System.Drawing.Point(506, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 20);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            //
+            // cb_TargetImageFolder
+            //
+            this.cb_TargetImageFolder.FormattingEnabled = true;
+            this.cb_TargetImageFolder.Location = new System.Drawing.Point(170, 23);
+            this.cb_TargetImageFolder.Name = "cb_TargetImageFolder";
+            this.cb_TargetImageFolder.Size = new System.Drawing.Size(184, 20);
+            this.cb_TargetImageFolder.TabIndex = 0;
+            //
+            // groupBox2
+            //
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.btn_SelectOutputFolder);
+            this.groupBox2.Location = new System.Drawing.Point(3, 6);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(624, 156);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "● Image Save Folder";
             //
             // label2
             //
@@ -186,12 +177,21 @@ namespace ITM_Agent.ucPanel
             this.label2.TabIndex = 13;
             this.label2.Text = "label2";
             //
-            // ucScreen3
+            // btn_SelectOutputFolder
+            //
+            this.btn_SelectOutputFolder.Location = new System.Drawing.Point(373, 65);
+            this.btn_SelectOutputFolder.Name = "btn_SelectOutputFolder";
+            this.btn_SelectOutputFolder.Size = new System.Drawing.Size(245, 28);
+            this.btn_SelectOutputFolder.TabIndex = 5;
+            this.btn_SelectOutputFolder.Text = "Select Folder";
+            this.btn_SelectOutputFolder.UseVisualStyleBackColor = true;
+            //
+            // ucImageTransPanel
             //
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.splitContainer2);
-            this.Name = "ucScreen3";
+            this.Name = "ucImageTransPanel";
             this.Size = new System.Drawing.Size(676, 310);
             this.splitContainer2.Panel1.ResumeLayout(false);
             this.splitContainer2.Panel2.ResumeLayout(false);
