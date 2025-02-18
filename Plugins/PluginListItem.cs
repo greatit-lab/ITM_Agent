2b85c3caf80e0395c531d9c46091b998526eaa94
greatit-lab <gil2zum@gmail.com>
2025-02-18 15:37:08 +0900

    Create PluginListItem.cs

---
+++ Plugins/PluginListItem.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ITM_Agent.Plugins
+{
+    public class PluginListItem
+    {
+        public string PluginName { get; set; }
+        public string AssemblyPath { get; set; }
+        public override string ToString() => PluginName;
+    }
+}
