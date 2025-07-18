b4805803c7fa6c46a163977eeee3fbac6737c124
greatit-lab <gil2zum@gmail.com>
2025-07-18 13:11:36 +0900

    Update PluginListItem.cs -- 0718_13

--- Plugins/PluginListItem.cs
+++ Plugins/PluginListItem.cs
@@ -1,5 +1,9 @@
 // Plugins\PluginListItem.cs
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ITM_Agent.Plugins
 {
@@ -7,6 +11,14 @@ namespace ITM_Agent.Plugins
     {
         public string PluginName { get; set; }
         public string AssemblyPath { get; set; }
-        public override string ToString() => PluginName;
+        public string PluginVersion { get; set; }    // ★ 추가: 어셈블리 버전 저장
+
+        // ☆ 변경: 이름만 리턴하던 ToString()을 버전 포함 형태로 수정
+        public override string ToString()
+        {
+            return string.IsNullOrEmpty(PluginVersion)
+                ? PluginName
+                : $"{PluginName} (v{PluginVersion})";
+        }
     }
 }
