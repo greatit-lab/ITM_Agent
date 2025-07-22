08e853c369e34d1983928bd269ddd5a41d36cf68
greatit-lab <gil2zum@gmail.com>
2025-07-22 16:50:39 +0900

    Update ucUploadPanel.Designer.cs

--- ucPanel/ucUploadPanel.Designer.cs
+++ ucPanel/ucUploadPanel.Designer.cs
@@ -38,6 +38,23 @@ namespace ITM_Agent.ucPanel
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_WaveClear = new System.Windows.Forms.Button();
+            this.label12 = new System.Windows.Forms.Label();
+            this.cb_WavePlugin = new System.Windows.Forms.ComboBox();
+            this.cb_WavePath = new System.Windows.Forms.ComboBox();
+            this.btn_WaveSet = new System.Windows.Forms.Button();
+            this.btn_EvClear = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.cb_EvPlugin = new System.Windows.Forms.ComboBox();
+            this.cb_EvPath = new System.Windows.Forms.ComboBox();
+            this.btn_EvSet = new System.Windows.Forms.Button();
+            this.btn_ErrClear = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.cb_ErrPlugin = new System.Windows.Forms.ComboBox();
+            this.cb_ErrPath = new System.Windows.Forms.ComboBox();
+            this.btn_ErrSet = new System.Windows.Forms.Button();
+            this.btn_ImgClear = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
             this.btn_PreAlignClear = new System.Windows.Forms.Button();
             this.label8 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
@@ -57,24 +74,6 @@ namespace ITM_Agent.ucPanel
             this.btn_FlatClear = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
-            
-            this.btn_ImgClear = new System.Windows.Forms.Button();
-            this.btn_ErrClear = new System.Windows.Forms.Button();
-            this.label10 = new System.Windows.Forms.Label();
-            this.cb_ErrPlugin = new System.Windows.Forms.ComboBox();
-            this.cb_ErrPath = new System.Windows.Forms.ComboBox();
-            this.btn_ErrSet = new System.Windows.Forms.Button();
-            this.btn_EvClear = new System.Windows.Forms.Button();
-            this.label11 = new System.Windows.Forms.Label();
-            this.cb_EvPlugin = new System.Windows.Forms.ComboBox();
-            this.cb_EvPath = new System.Windows.Forms.ComboBox();
-            this.btn_EvSet = new System.Windows.Forms.Button();
-            this.btn_WaveClear = new System.Windows.Forms.Button();
-            this.label12 = new System.Windows.Forms.Label();
-            this.cb_WavePlugin = new System.Windows.Forms.ComboBox();
-            this.cb_WavePath = new System.Windows.Forms.ComboBox();
-            this.btn_WaveSet = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             //
@@ -122,6 +121,157 @@ namespace ITM_Agent.ucPanel
             this.groupBox1.TabIndex = 18;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "● Database Uploading";
+            //
+            // btn_WaveClear
+            //
+            this.btn_WaveClear.Location = new System.Drawing.Point(543, 248);
+            this.btn_WaveClear.Name = "btn_WaveClear";
+            this.btn_WaveClear.Size = new System.Drawing.Size(75, 22);
+            this.btn_WaveClear.TabIndex = 53;
+            this.btn_WaveClear.Text = "Clear";
+            this.btn_WaveClear.UseVisualStyleBackColor = true;
+            // 
+            // label12
+            // 
+            this.label12.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label12.Font = new System.Drawing.Font("굴림", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label12.Location = new System.Drawing.Point(253, 251);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(25, 22);
+            this.label12.TabIndex = 52;
+            this.label12.Text = "→";
+            //
+            // cb_WavePlugin
+            //
+            this.cb_WavePlugin.FormattingEnabled = true;
+            this.cb_WavePlugin.Location = new System.Drawing.Point(283, 249);
+            this.cb_WavePlugin.Name = "cb_WavePlugin";
+            this.cb_WavePlugin.Size = new System.Drawing.Size(175, 20);
+            this.cb_WavePlugin.TabIndex = 51;
+            //
+            // cb_WavePath
+            //
+            this.cb_WavePath.FormattingEnabled = true;
+            this.cb_WavePath.Location = new System.Drawing.Point(30, 249);
+            this.cb_WavePath.Name = "cb_WavePath";
+            this.cb_WavePath.Size = new System.Drawing.Size(214, 20);
+            this.cb_WavePath.TabIndex = 49;
+            //
+            // btn_WaveSet
+            //
+            this.btn_WaveSet.Location = new System.Drawing.Point(465, 248);
+            this.btn_WaveSet.Name = "btn_WaveSet";
+            this.btn_WaveSet.Size = new System.Drawing.Size(75, 22);
+            this.btn_WaveSet.TabIndex = 50;
+            this.btn_WaveSet.Text = "Set";
+            this.btn_WaveSet.UseVisualStyleBackColor = true;
+            //
+            // btn_EvClear
+            //
+            this.btn_EvClear.Location = new System.Drawing.Point(543, 206);
+            this.btn_EvClear.Name = "btn_EvClear";
+            this.btn_EvClear.Size = new System.Drawing.Size(75, 22);
+            this.btn_EvClear.TabIndex = 48;
+            this.btn_EvClear.Text = "Clear";
+            this.btn_EvClear.UseVisualStyleBackColor = true;
+            // 
+            // label11
+            // 
+            this.label11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label11.Font = new System.Drawing.Font("굴림", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label11.Location = new System.Drawing.Point(253, 209);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(25, 22);
+            this.label11.TabIndex = 47;
+            this.label11.Text = "→";
+            //
+            // cb_EvPlugin
+            //
+            this.cb_EvPlugin.FormattingEnabled = true;
+            this.cb_EvPlugin.Location = new System.Drawing.Point(283, 207);
+            this.cb_EvPlugin.Name = "cb_EvPlugin";
+            this.cb_EvPlugin.Size = new System.Drawing.Size(175, 20);
+            this.cb_EvPlugin.TabIndex = 46;
+            //
+            // cb_EvPath
+            //
+            this.cb_EvPath.FormattingEnabled = true;
+            this.cb_EvPath.Location = new System.Drawing.Point(30, 207);
+            this.cb_EvPath.Name = "cb_EvPath";
+            this.cb_EvPath.Size = new System.Drawing.Size(214, 20);
+            this.cb_EvPath.TabIndex = 44;
+            //
+            // btn_EvSet
+            //
+            this.btn_EvSet.Location = new System.Drawing.Point(465, 206);
+            this.btn_EvSet.Name = "btn_EvSet";
+            this.btn_EvSet.Size = new System.Drawing.Size(75, 22);
+            this.btn_EvSet.TabIndex = 45;
+            this.btn_EvSet.Text = "Set";
+            this.btn_EvSet.UseVisualStyleBackColor = true;
+            //
+            // btn_ErrClear
+            //
+            this.btn_ErrClear.Location = new System.Drawing.Point(543, 163);
+            this.btn_ErrClear.Name = "btn_ErrClear";
+            this.btn_ErrClear.Size = new System.Drawing.Size(75, 22);
+            this.btn_ErrClear.TabIndex = 43;
+            this.btn_ErrClear.Text = "Clear";
+            this.btn_ErrClear.UseVisualStyleBackColor = true;
+            // 
+            // label10
+            // 
+            this.label10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label10.Font = new System.Drawing.Font("굴림", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label10.Location = new System.Drawing.Point(253, 166);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(25, 22);
+            this.label10.TabIndex = 42;
+            this.label10.Text = "→";
+            //
+            // cb_ErrPlugin
+            //
+            this.cb_ErrPlugin.FormattingEnabled = true;
+            this.cb_ErrPlugin.Location = new System.Drawing.Point(283, 164);
+            this.cb_ErrPlugin.Name = "cb_ErrPlugin";
+            this.cb_ErrPlugin.Size = new System.Drawing.Size(175, 20);
+            this.cb_ErrPlugin.TabIndex = 41;
+            //
+            // cb_ErrPath
+            //
+            this.cb_ErrPath.FormattingEnabled = true;
+            this.cb_ErrPath.Location = new System.Drawing.Point(30, 164);
+            this.cb_ErrPath.Name = "cb_ErrPath";
+            this.cb_ErrPath.Size = new System.Drawing.Size(214, 20);
+            this.cb_ErrPath.TabIndex = 39;
+            //
+            // btn_ErrSet
+            //
+            this.btn_ErrSet.Location = new System.Drawing.Point(465, 163);
+            this.btn_ErrSet.Name = "btn_ErrSet";
+            this.btn_ErrSet.Size = new System.Drawing.Size(75, 22);
+            this.btn_ErrSet.TabIndex = 40;
+            this.btn_ErrSet.Text = "Set";
+            this.btn_ErrSet.UseVisualStyleBackColor = true;
+            //
+            // btn_ImgClear
+            //
+            this.btn_ImgClear.Location = new System.Drawing.Point(543, 120);
+            this.btn_ImgClear.Name = "btn_ImgClear";
+            this.btn_ImgClear.Size = new System.Drawing.Size(75, 22);
+            this.btn_ImgClear.TabIndex = 38;
+            this.btn_ImgClear.Text = "Clear";
+            this.btn_ImgClear.UseVisualStyleBackColor = true;
+            // 
+            // label9
+            // 
+            this.label9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label9.Font = new System.Drawing.Font("굴림", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label9.Location = new System.Drawing.Point(253, 123);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(25, 22);
+            this.label9.TabIndex = 37;
+            this.label9.Text = "→";
             //
             // btn_PreAlignClear
             //
@@ -289,157 +439,6 @@ namespace ITM_Agent.ucPanel
             this.label3.Size = new System.Drawing.Size(146, 22);
             this.label3.TabIndex = 7;
             this.label3.Text = "• Wafer Flat Data Path";
-            // 
-            // label9
-            // 
-            this.label9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.label9.Font = new System.Drawing.Font("굴림", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label9.Location = new System.Drawing.Point(253, 123);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(25, 22);
-            this.label9.TabIndex = 37;
-            this.label9.Text = "→";
-            //
-            // btn_ImgClear
-            //
-            this.btn_ImgClear.Location = new System.Drawing.Point(543, 120);
-            this.btn_ImgClear.Name = "btn_ImgClear";
-            this.btn_ImgClear.Size = new System.Drawing.Size(75, 22);
-            this.btn_ImgClear.TabIndex = 38;
-            this.btn_ImgClear.Text = "Clear";
-            this.btn_ImgClear.UseVisualStyleBackColor = true;
-            //
-            // btn_ErrClear
-            //
-            this.btn_ErrClear.Location = new System.Drawing.Point(543, 163);
-            this.btn_ErrClear.Name = "btn_ErrClear";
-            this.btn_ErrClear.Size = new System.Drawing.Size(75, 22);
-            this.btn_ErrClear.TabIndex = 43;
-            this.btn_ErrClear.Text = "Clear";
-            this.btn_ErrClear.UseVisualStyleBackColor = true;
-            // 
-            // label10
-            // 
-            this.label10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.label10.Font = new System.Drawing.Font("굴림", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label10.Location = new System.Drawing.Point(253, 166);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(25, 22);
-            this.label10.TabIndex = 42;
-            this.label10.Text = "→";
-            //
-            // cb_ErrPlugin
-            //
-            this.cb_ErrPlugin.FormattingEnabled = true;
-            this.cb_ErrPlugin.Location = new System.Drawing.Point(283, 164);
-            this.cb_ErrPlugin.Name = "cb_ErrPlugin";
-            this.cb_ErrPlugin.Size = new System.Drawing.Size(175, 20);
-            this.cb_ErrPlugin.TabIndex = 41;
-            //
-            // cb_ErrPath
-            //
-            this.cb_ErrPath.FormattingEnabled = true;
-            this.cb_ErrPath.Location = new System.Drawing.Point(30, 164);
-            this.cb_ErrPath.Name = "cb_ErrPath";
-            this.cb_ErrPath.Size = new System.Drawing.Size(214, 20);
-            this.cb_ErrPath.TabIndex = 39;
-            //
-            // btn_ErrSet
-            //
-            this.btn_ErrSet.Location = new System.Drawing.Point(465, 163);
-            this.btn_ErrSet.Name = "btn_ErrSet";
-            this.btn_ErrSet.Size = new System.Drawing.Size(75, 22);
-            this.btn_ErrSet.TabIndex = 40;
-            this.btn_ErrSet.Text = "Set";
-            this.btn_ErrSet.UseVisualStyleBackColor = true;
-            //
-            // btn_EvClear
-            //
-            this.btn_EvClear.Location = new System.Drawing.Point(543, 206);
-            this.btn_EvClear.Name = "btn_EvClear";
-            this.btn_EvClear.Size = new System.Drawing.Size(75, 22);
-            this.btn_EvClear.TabIndex = 48;
-            this.btn_EvClear.Text = "Clear";
-            this.btn_EvClear.UseVisualStyleBackColor = true;
-            // 
-            // label11
-            // 
-            this.label11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.label11.Font = new System.Drawing.Font("굴림", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label11.Location = new System.Drawing.Point(253, 209);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(25, 22);
-            this.label11.TabIndex = 47;
-            this.label11.Text = "→";
-            //
-            // cb_EvPlugin
-            //
-            this.cb_EvPlugin.FormattingEnabled = true;
-            this.cb_EvPlugin.Location = new System.Drawing.Point(283, 207);
-            this.cb_EvPlugin.Name = "cb_EvPlugin";
-            this.cb_EvPlugin.Size = new System.Drawing.Size(175, 20);
-            this.cb_EvPlugin.TabIndex = 46;
-            //
-            // cb_EvPath
-            //
-            this.cb_EvPath.FormattingEnabled = true;
-            this.cb_EvPath.Location = new System.Drawing.Point(30, 207);
-            this.cb_EvPath.Name = "cb_EvPath";
-            this.cb_EvPath.Size = new System.Drawing.Size(214, 20);
-            this.cb_EvPath.TabIndex = 44;
-            //
-            // btn_EvSet
-            //
-            this.btn_EvSet.Location = new System.Drawing.Point(465, 206);
-            this.btn_EvSet.Name = "btn_EvSet";
-            this.btn_EvSet.Size = new System.Drawing.Size(75, 22);
-            this.btn_EvSet.TabIndex = 45;
-            this.btn_EvSet.Text = "Set";
-            this.btn_EvSet.UseVisualStyleBackColor = true;
-            //
-            // btn_WaveClear
-            //
-            this.btn_WaveClear.Location = new System.Drawing.Point(543, 248);
-            this.btn_WaveClear.Name = "btn_WaveClear";
-            this.btn_WaveClear.Size = new System.Drawing.Size(75, 22);
-            this.btn_WaveClear.TabIndex = 53;
-            this.btn_WaveClear.Text = "Clear";
-            this.btn_WaveClear.UseVisualStyleBackColor = true;
-            // 
-            // label12
-            // 
-            this.label12.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.label12.Font = new System.Drawing.Font("굴림", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label12.Location = new System.Drawing.Point(253, 251);
-            this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(25, 22);
-            this.label12.TabIndex = 52;
-            this.label12.Text = "→";
-            //
-            // cb_WavePlugin
-            //
-            this.cb_WavePlugin.FormattingEnabled = true;
-            this.cb_WavePlugin.Location = new System.Drawing.Point(283, 249);
-            this.cb_WavePlugin.Name = "cb_WavePlugin";
-            this.cb_WavePlugin.Size = new System.Drawing.Size(175, 20);
-            this.cb_WavePlugin.TabIndex = 51;
-            //
-            // cb_WavePath
-            //
-            this.cb_WavePath.FormattingEnabled = true;
-            this.cb_WavePath.Location = new System.Drawing.Point(30, 249);
-            this.cb_WavePath.Name = "cb_WavePath";
-            this.cb_WavePath.Size = new System.Drawing.Size(214, 20);
-            this.cb_WavePath.TabIndex = 49;
-            //
-            // btn_WaveSet
-            //
-            this.btn_WaveSet.Location = new System.Drawing.Point(465, 248);
-            this.btn_WaveSet.Name = "btn_WaveSet";
-            this.btn_WaveSet.Size = new System.Drawing.Size(75, 22);
-            this.btn_WaveSet.TabIndex = 50;
-            this.btn_WaveSet.Text = "Set";
-            this.btn_WaveSet.UseVisualStyleBackColor = true;
             //
             // ucUploadPanel
             //
